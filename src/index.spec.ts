db8a6a9140dcbe86ac73d9f693ff24d18cb60e11
Daniel Sitek <dan.sitek@gmail.com>
2025-02-25 21:15:18 +0100

    created basic tests

---
+++ src/index.spec.ts
@@ -0,0 +1,51 @@
+import { assert, expect, describe, it } from 'vitest';
+import { gulpTateru } from './index';
+const gulp = require('gulp');
+
+describe('gulpTateru', () => {
+  it('should be a function', () => {
+    assert.isFunction(gulpTateru);
+  });
+
+  it('should generate files', async () => {
+    const i = await new Promise((resolve) => {
+      let n = 0;
+
+      gulp
+        .src('./tateru.config.json', { cwd: 'test/fixtures' })
+        .pipe(gulpTateru())
+        .on('data', () => {
+          n++;
+        })
+        .on('end', () => {
+          resolve(n);
+        });
+    });
+
+    expect(i).toBe(6);
+  });
+
+  it('should generate selected file', async () => {
+    const { count, generatedFile } = await new Promise<{
+      count: number;
+      generatedFile: string;
+    }>((resolve) => {
+      let n = 0;
+      let generatedFile;
+
+      gulp
+        .src('./tateru.config.json', { cwd: 'test/fixtures' })
+        .pipe(gulpTateru({ page: 'about' }))
+        .on('data', (file) => {
+          n++;
+          generatedFile = file.contents.toString();
+        })
+        .on('end', () => {
+          resolve({ count: n, generatedFile });
+        });
+    });
+
+    expect(count).toBe(1);
+    expect(generatedFile).toContain('<h2>Page About</h2>');
+  });
+});
